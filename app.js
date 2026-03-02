46881ca345440d67c148b71d1b42f9fed3ff78a2
Lenovo <vaibhavkanade03@gmail.com>
2026-03-02 17:04:41 +0530

    Added middleware

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(require('./middleware/security'));
 
 /* ==============================
    DATABASE CONNECTION (CRITICAL)
